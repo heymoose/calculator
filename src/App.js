58066b948eb9d6f99899d30e0b3d5155a6ec64e3
heymoose <marcusanovak@gmail.com>
2019-01-28 14:49:29 -0700

    Add calculator component and tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Calculator from './containers/Calculator/Calculator';
 
 class App extends Component {
     render() {
-        return <div className='App' />;
+        return (
+            <div className='app-container'>
+                <Calculator />
+            </div>
+        );
     }
 }
 
---
+++ src/App.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Calculator from './containers/Calculator/Calculator';
+
+describe('App', () => {
+    let wrapper;
+    beforeEach(() => (wrapper = shallow(<App />)));
+
+    it('should render correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a <div />', () => {
+        expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it('should render the Calculator component', () => {
+        expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
+    });
+});
