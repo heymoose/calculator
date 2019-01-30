fa5fe971d3e5103c3cdadd8f2f3b66f4e8ce89ae
heymoose <marcusanovak@gmail.com>
2019-01-29 18:56:39 -0700

    Add tests and functionality for click events

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -14,15 +14,71 @@ class Calculator extends Component {
     };
 
     callOperator = () => {
-        console.log('call operation');
+        let { displayValue, selectedOperator, storedValue } = this.state;
+        const updateStoredValue = displayValue;
+
+        displayValue = parseInt(displayValue, 10);
+        storedValue = parseInt(storedValue, 10);
+
+        switch (selectedOperator) {
+            case '+':
+                displayValue = storedValue + displayValue;
+                break;
+            case '-':
+                displayValue = storedValue - displayValue;
+                break;
+            case 'x':
+                displayValue = storedValue * displayValue;
+                break;
+            case '/':
+                displayValue = storedValue / displayValue;
+                break;
+            default:
+                displayValue = '0';
+        }
+
+        displayValue = displayValue.toString();
+        selectedOperator = '';
+
+        if (displayValue === 'NaN' || displayValue === 'Infinity') {
+            displayValue = '0';
+        }
+
+        this.setState({ displayValue, selectedOperator, storedValue: updateStoredValue });
     };
 
-    setOperator = () => {
-        console.log('set operation');
+    setOperator = value => {
+        let { displayValue, selectedOperator, storedValue } = this.state;
+
+        if (selectedOperator === '') {
+            storedValue = displayValue;
+            displayValue = '0';
+            selectedOperator = value;
+        } else {
+            selectedOperator = value;
+        }
+
+        this.setState({ storedValue, displayValue, selectedOperator });
     };
 
-    updateDisplay = () => {
-        console.log('update display');
+    updateDisplay = value => {
+        let { displayValue } = this.state;
+
+        if (value === '.' && displayValue.includes('.')) {
+            value = '';
+        }
+
+        if (value === 'ce') {
+            displayValue = displayValue.substr(0, displayValue.length - 1);
+
+            if (displayValue === '') {
+                displayValue = '0';
+            }
+        } else {
+            displayValue = displayValue === '0' ? value : displayValue + value;
+        }
+
+        this.setState({ displayValue });
     };
 
     render() {
